4f11bc58961d60c6ab3601cc43c5f2ff60b62772
Doug <douglaswatson@gmail.com>
2017-05-03 21:27:04 -0500

    adding my fizzbuzz program

---
+++ interveiw.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	for i := 0; i < 100; i++ {
+		x := i % 3
+		y := i % 5
+		if x == 0 && y == 0 {
+			println("fizzbuzz")
+		} else if y == 0 {
+			println("buzz")
+		} else if x == 0 {
+			println("fizz")
+		} else {
+			println(i)
+		}
+	}
+}
